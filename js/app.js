3b6ccb06a210308c63162749f7e3aa657afaa531
Sean Reigle <sereigle@gmail.com>
2017-03-14 16:19:55 -0700

    Tuesday - Lab

--- js/app.js
+++ js/app.js
@@ -1,19 +1,21 @@
 'use strict';
 
-var storeHours = ['6am','7am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','total'];
+var storeHours = ['6am','7am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm','Total'];
 
 var body = document.getElementsByTagName('body')[0];
 
-var firstAndPike = {
-  minCust: 23,
-  maxCust: 65,
-  avgCookie: 6.3,
-  salesArray:[],
-  name: '1st and Pike',
-  randCust: function() {
+function cookieShops (minCust, maxCust, avgCookie, name) {
+  this.minCust = minCust;
+  this.maxCust = maxCust;
+  this.avgCookie = avgCookie;
+  this.name = name;
+  this.salesArray = [];
+
+  this.randCust = function() {
     return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  cookieSales: function() {
+  };
+
+  this.cookieSales = function() {
     var total = 0;
     for (var i = 0; i < storeHours.length - 1; i++) {
       var salesPerhour = Math.floor(this.randCust() * this.avgCookie);
@@ -21,9 +23,9 @@ var firstAndPike = {
       total += salesPerhour;
     }
     this.salesArray.push(total);
-  },
-  createListItems: function(){
-    this.cookieSales();
+  };
+
+  this.createListItems = function(){
     var newHeading = document.createElement('h2');
     body.appendChild(newHeading);
     newHeading.innerText = this.name;
@@ -34,147 +36,10 @@ var firstAndPike = {
       newUL.appendChild(newLI);
     }
     body.appendChild(newUL);
-  },
+  };
 };
-
-firstAndPike.createListItems();
-
-var seaTacAir = {
-  minCust: 3,
-  maxCust: 24,
-  avgCookie: 1.2,
-  salesArray:[],
-  name: 'SeaTac Airport',
-  randCust: function() {
-    return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  cookieSales: function() {
-    var total = 0;
-    for (var i = 0; i < storeHours.length - 1; i++) {
-      var salesPerhour = Math.floor(this.randCust() * this.avgCookie);
-      this.salesArray.push(salesPerhour);
-      total += salesPerhour;
-    }
-    this.salesArray.push(total);
-  },
-  createListItems: function(){
-    this.cookieSales();
-    var newHeading = document.createElement('h2');
-    body.appendChild(newHeading);
-    newHeading.innerText = this.name;
-    var newUL = document.createElement('ul');
-    for (var i = 0; i < storeHours.length; i++) {
-      var newLI = document.createElement('li');
-      newLI.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-      newUL.appendChild(newLI);
-    }
-    body.appendChild(newUL);
-  },
-};
-
-seaTacAir.createListItems();
-
-var seattleCenter = {
-  minCust: 11,
-  maxCust: 38,
-  avgCookie: 3.7,
-  salesArray:[],
-  name: 'Seattle Center',
-  randCust: function() {
-    return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  cookieSales: function() {
-    var total = 0;
-    for (var i = 0; i < storeHours.length - 1; i++) {
-      var salesPerhour = Math.floor(this.randCust() * this.avgCookie);
-      this.salesArray.push(salesPerhour);
-      total += salesPerhour;
-    }
-    this.salesArray.push(total);
-  },
-  createListItems: function(){
-    this.cookieSales();
-    var newHeading = document.createElement('h2');
-    body.appendChild(newHeading);
-    newHeading.innerText = this.name;
-    var newUL = document.createElement('ul');
-    for (var i = 0; i < storeHours.length; i++) {
-      var newLI = document.createElement('li');
-      newLI.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-      newUL.appendChild(newLI);
-    }
-    body.appendChild(newUL);
-  },
-};
-
-seattleCenter.createListItems();
-
-var capHill = {
-  minCust: 20,
-  maxCust: 38,
-  avgCookie: 2.3,
-  salesArray:[],
-  name: 'Capitol Hill',
-  randCust: function() {
-    return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  cookieSales: function() {
-    var total = 0;
-    for (var i = 0; i < storeHours.length - 1; i++) {
-      var salesPerhour = Math.floor(this.randCust() * this.avgCookie);
-      this.salesArray.push(salesPerhour);
-      total += salesPerhour;
-    }
-    this.salesArray.push(total);
-  },
-  createListItems: function(){
-    this.cookieSales();
-    var newHeading = document.createElement('h2');
-    body.appendChild(newHeading);
-    newHeading.innerText = this.name;
-    var newUL = document.createElement('ul');
-    for (var i = 0; i < storeHours.length; i++) {
-      var newLI = document.createElement('li');
-      newLI.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-      newUL.appendChild(newLI);
-    }
-    body.appendChild(newUL);
-  },
-};
-
-capHill.createListItems();
-
-var alki = {
-  minCust: 2,
-  maxCust: 16,
-  avgCookie: 4.6,
-  salesArray:[],
-  name: 'Alki',
-  randCust: function() {
-    return Math.floor (Math.random() * (this.maxCust - this.minCust + 1) + this.minCust);
-  },
-  cookieSales: function() {
-    var total = 0;
-    for (var i = 0; i < storeHours.length - 1; i++) {
-      var salesPerhour = Math.floor(this.randCust() * this.avgCookie);
-      this.salesArray.push(salesPerhour);
-      total += salesPerhour;
-    }
-    this.salesArray.push(total);
-  },
-  createListItems: function(){
-    this.cookieSales();
-    var newHeading = document.createElement('h2');
-    body.appendChild(newHeading);
-    newHeading.innerText = this.name;
-    var newUL = document.createElement('ul');
-    for (var i = 0; i < storeHours.length; i++) {
-      var newLI = document.createElement('li');
-      newLI.innerText = storeHours[i] + ': ' + this.salesArray[i] + ' cookies';
-      newUL.appendChild(newLI);
-    }
-    body.appendChild(newUL);
-  },
-};
-
-alki.createListItems();
+var shopsOne = new cookieShops(23, 65, 6.3,'1st and Pike');
+var shopsTwo = new cookieShops(3, 24, 1.2,'SeaTac Airport');
+var shopsThree = new cookieShops(11, 38, 3.7,'Seattle Center');
+var shopsFour = new cookieShops(20, 38, 2.3,'Capitol Hill');
+var shopsFive = new cookieShops(2, 16, 4.6,'Alki');
